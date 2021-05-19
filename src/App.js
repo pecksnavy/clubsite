bc4aa3b42b1e9e03b1890a241e9b7507a28d3ae1
Brian Thornton <brianthornton@brians-mbp.lan>
2021-05-19 08:46:01 -0400

    Updates

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          PECK's NAVY - Ocean City NJ's Boating Club
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/groups/2322729067827878"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Join the crew on Facebook
         </a>
       </header>
     </div>
